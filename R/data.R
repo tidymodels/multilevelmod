98a5e0f233dce0973c1c44e9b8082f0604d6b448
topepo <mxkuhn@gmail.com>
2020-04-28 12:35:43 -0400

    data sets for logistic regression

--- R/data.R
+++ R/data.R
@@ -38,3 +38,49 @@ NULL
 #' data(riesby)
 #' str(riesby)
 NULL
+
+#' Simulated lung cancer data
+#'
+#' @details A simulated data set with a binary outcome with levels "0" and "1".
+#' Created by the UCLA Statistical Consulting group.
+#'
+#' @name hdp
+#' @aliases hdp
+#' @docType data
+#' @return \item{hdp}{a tibble}
+#'
+#' @source
+#' \url{https://stats.idre.ucla.edu/r/dae/mixed-effects-logistic-regression/},
+#' \url{https://stats.idre.ucla.edu/stat/data/hdp.csv}
+#'
+#'
+#'
+#' @keywords datasets
+#' @examples
+#' data(hdp)
+#' str(hdp)
+NULL
+
+#' Baseball hits
+#'
+#' @details From website of Jerry Brunner:
+#'
+#' "Right-handed basketball players take right and left-handed shots from 3
+#'  locations in a different random order for each player. Hit or miss is
+#'  recorded. This is a 2x3 factorial design with repeated measures on both
+#'  factors: Hand they are shooting with and spot on the court."
+#'
+#' @name baseball
+#' @aliases baseball
+#' @docType data
+#' @return \item{baseball}{a tibble}
+#'
+#' @source
+#' \url{http://www.utstat.toronto.edu/~brunner/workshops/mixed/BinaryBballWithR.pdf}
+#'
+#' @keywords datasets
+#' @examples
+#' data(baseball)
+#' str(baseball)
+NULL
+
