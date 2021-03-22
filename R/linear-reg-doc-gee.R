72d3829d3552f7c475deda7b811f4e841957f60b
Max Kuhn <mxkuhn@gmail.com>
2021-03-22 12:28:19 -0400

    example doc file to use with new parsnip documentation prototype

---
+++ R/linear-reg-doc-gee.R
@@ -0,0 +1,10 @@
+#' Linear regression via generalized estimating equations (GEE)
+#'
+#' `gee::gee()` uses generalized least squares to fit different types of models
+#' that have errors that are not independent.
+#'
+#' @includeRmd man/rmd/linear-reg-gee.Rmd details
+#'
+#' @name details-linear-reg-gee
+#' @keywords internal
+NULL
