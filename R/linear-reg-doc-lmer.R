13b83b9f06e2c34c15f560ae0ba74dbc447cc365
Max Kuhn <mxkuhn@gmail.com>
2021-03-22 18:23:44 -0400

    more documentation files

---
+++ R/linear-reg-doc-lmer.R
@@ -0,0 +1,10 @@
+#' Linear regression via mixed models
+#'
+#' The `lmer` engine estimates fixed and random effect regression parameters
+#' using maximum likelihood (or restricted maximum likelihood) estimation.
+#'
+#' @includeRmd man/rmd/linear-reg-lmer.Rmd details
+#'
+#' @name details-linear-reg-lmer
+#' @keywords internal
+NULL
---
+++ R/linear-reg-doc-stan-glmer.R
@@ -0,0 +1,10 @@
+#' Linear regression via hierarchical Bayesian methods
+#'
+#' The `stan-glmer` engine estimates hierarchical regression parameters using
+#' Bayesian estimation.
+#'
+#' @includeRmd man/rmd/linear-reg-stan-glmer.Rmd details
+#'
+#' @name details-linear-reg-stan-glmer
+#' @keywords internal
+NULL
