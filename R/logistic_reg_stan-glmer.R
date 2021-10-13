fd8b39caacb498d68f7a6a3811c434f38551e307
Max Kuhn <mxkuhn@gmail.com>
2021-10-13 14:42:46 -0400

    Rmd for engine docs

---
+++ R/logistic_reg_stan-glmer.R
@@ -0,0 +1,10 @@
+#' Logistic regression via hierarchical Bayesian methods
+#'
+#' The `stan-glmer` engine estimates hierarchical regression parameters using
+#' Bayesian estimation.
+#'
+#' @includeRmd man/rmd/logistic_reg_stan-glmer.Rmd details
+#'
+#' @name details_logistic_reg_stan-glmer
+#' @keywords internal
+NULL
